596abf37ac0cdd8fe741bb20716e71ef028635b8
Amanda <31295398+anguye25@users.noreply.github.com>
2017-11-29 17:17:31 -0700

    Initial Commit

---
+++ BaroMyBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaroMyBuddy
+//
+//  Created by Amanda Nguyen on 11/29/17.
+//  Copyright © 2017 Amanda Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
